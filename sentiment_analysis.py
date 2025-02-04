7adb02e60f6359d2ecaaeef4c9614fb7561df7ce
aarsh <aarshajay.sheth@sjsu.edu>
2025-02-03 20:44:24 -0800

    optimize code

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,29 +1,3 @@
-# import pandas as pd
-# from textblob import TextBlob
-#
-# # Load CSV file
-# df = pd.read_csv("input_test.csv", header=None)
-#
-# # Assuming the tweet text is in the 6th column (index 5)
-# df["Sentiment"] = df[5].apply(lambda x: TextBlob(str(x)).sentiment.polarity)
-#
-# # Function to categorize sentiment
-# def categorize_sentiment(score):
-#     if score > 0:
-#         return "Positive"
-#     elif score < 0:
-#         return "Negative"
-#     else:
-#         return "Neutral"
-#
-# df["Sentiment_Label"] = df["Sentiment"].apply(categorize_sentiment)
-#
-# # Save the results to an Excel file
-# df[[5, "Sentiment_Label"]].to_excel("sentiment_results.xlsx", index=False, header=["Text", "Sentiment"])
-#
-# print("Sentiment analysis completed! Check sentiment_results.xlsx")
-
-
 import pandas as pd
 from textblob import TextBlob
 
@@ -35,12 +9,7 @@ df["Sentiment"] = df[5].apply(lambda x: TextBlob(str(x)).sentiment.polarity)
 
 # Function to categorize sentiment
 def categorize_sentiment(score):
-    if score > 0:
-        return "Positive"
-    elif score < 0:
-        return "Negative"
-    else:
-        return "Neutral"
+    return "Positive" if score > 0 else "Negative" if score < 0 else "Neutral"
 
 df["Sentiment_Label"] = df["Sentiment"].apply(categorize_sentiment)
 
